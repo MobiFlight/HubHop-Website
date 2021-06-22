c5f01d75f630f9df802efb821203631cc323157a
rofl-er <sergej.weber02@gmail.com>
2021-06-22 19:39:10 +0200

    fixed font-family not being loaded

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,7 @@ export default {
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=KoHo:wght@300&display=swap');
 html {
   height: 100%;
   margin: 0;
@@ -27,7 +28,7 @@ html {
 }
 
 #app {
-  font-family: KoHo Light, Helvetica, Arial, sans-serif;
+  font-family: 'KoHo', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
