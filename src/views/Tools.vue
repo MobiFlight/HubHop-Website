47b909638c6f2ad7edfc93c6524d2ab39893cdfc
rofl-er <sergej.weber02@gmail.com>
2021-12-21 17:58:28 +0100

    removed icons from placeholders

--- src/views/Tools.vue
+++ src/views/Tools.vue
@@ -34,6 +34,7 @@
       </button>
       <button
         @click="toggleJaime"
+        disabled
         class="
           bg-hhCard
           text-hhText
@@ -44,24 +45,7 @@
           rounded-box
         "
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="icon mr-1 icon-tabler icon-tabler-replace"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-          <rect x="3" y="3" width="6" height="6" rx="1"></rect>
-          <rect x="15" y="15" width="6" height="6" rx="1"></rect>
-          <path d="M21 11v-3a2 2 0 0 0 -2 -2h-6l3 3m0 -6l-3 3"></path>
-          <path d="M3 13v3a2 2 0 0 0 2 2h6l-3 -3m0 6l3 -3"></path></svg
-        >Placeholder
+        Placeholder
       </button>
       <button
         disabled
@@ -76,24 +60,7 @@
           rounded-box
         "
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="icon mr-1 icon-tabler icon-tabler-replace"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-          <rect x="3" y="3" width="6" height="6" rx="1"></rect>
-          <rect x="15" y="15" width="6" height="6" rx="1"></rect>
-          <path d="M21 11v-3a2 2 0 0 0 -2 -2h-6l3 3m0 -6l-3 3"></path>
-          <path d="M3 13v3a2 2 0 0 0 2 2h6l-3 -3m0 6l3 -3"></path></svg
-        >Placeholder
+        Placeholder
       </button>
     </div>
     <div>
